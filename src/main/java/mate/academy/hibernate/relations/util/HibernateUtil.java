abbf8012afd4be7777eab52031f4a3f7deaa95e6
kastashenkova <astashenkova.katya@gmail.com>
2025-12-25 22:07:33 +0200

    relations and hibernate

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,24 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory instance = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Could not initialize Hibernate SessionFactory", e);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
     }
 }
